7a98f0d2cfe9c122f66b61d9c493c7c264c98758
Dempsey <dempsey.rhian.neal@gmail.com>
2021-03-11 18:45:40 +0000

    update App.js week-10 file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import Squares from './components/lifting-state';
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+import Articles from './news/Articles';
+import ArticleIdNumbers from './news/ArticleIdNumbers';
+import Clicked from './hooks/Clicked';
+import Square from './hooks/Square';
+import Toggle from './hooks/Toggle';
+import Counter from './hooks/Counter';
+import StepCounter from './hooks/StepCounter';
+import TempConverter from './hooks/TempConverter';
+import List from './hooks/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+
+    <Router>
+      <>
+          {/* <Route exact path="/">
+          <Squares color="hotpink" />    
+          </Route>       */}
+          
+  
+          <Route exact path="/news">
+            <Articles />
+          </Route>
+
+          {/* <Route exact path="/news">
+            <ArticlesIdNumbers />
+          </Route> */}
+
+          <Route exact path="/hooks">
+            <Clicked />
+            <Square color="hotpink" />    
+            <Counter max={100} /> 
+            <StepCounter max={100} step={5}/>  
+            <TempConverter />
+            <List />
+          </Route>  
+
+        </>
+      </Router>
+)
+
+    
 
 export default App;
